3a69f4d099743b7041dc75a495d119c4f340750d
Enes Karakuş <eneskarakus44@outlook.com>
2023-10-04 17:45:52 +0300

    Update

--- database/migrations/2023_10_04_130035_create_companies_table.php
+++ database/migrations/2023_10_04_130035_create_companies_table.php
@@ -4,30 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->string('short_name', 30);
-            $table->string('full_name', 200);
-            $table->string('email', 100);
-            $table->string('phone', 12);
-            $table->string('fax', 12);
+            $table->string('title', 255);
+            $table->boolean('is_person');
+            $table->string('first_name', 255)->nullable();
+            $table->string('last_name', 255)->nullable();
+            $table->string('tax_number', 10);
+            $table->string('identity_number', 11);
             $table->string('address', 255);
             $table->unsignedBigInteger('city_id');
             $table->unsignedBigInteger('town_id');
             $table->unsignedBigInteger('country_id');
             $table->unsignedBigInteger('tax_office_id');
-            $table->string('tax_number', 11);
-            $table->boolean('is_person');
-            $table->boolean('is_active');
-            $table->boolean('is_create_balance');
-            $table->decimal('is_creating_balance_amount', 10, 2);
+            $table->string('email', 200);
+            $table->string('phone', 30);
+            $table->string('fax', 30);
             $table->timestamps();
             $table->softDeletes();
         });
@@ -38,6 +36,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('companies');
     }
 };
